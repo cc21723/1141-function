da1db9d96f3c0a8f684c0d7ff04244e44a48ad96
mackliu <macklun@gmail.com>
2025-06-04 16:06:23 +0800

    使用自訂函式來整合各種星星畫圖

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'library.php'; ?>
+<h2>正三角形</h2>
+<style>
+    *{
+        font-family:'Courier New', Courier, monospace;
+    }
+</style>
+<?php
+
+
+stars('正三角形', 15);
+stars('菱形', 15);
+stars('矩形', 15);
+stars('倒三角形', 15);
+
+
